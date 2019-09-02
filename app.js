f154b99357ab7a0b15e304b633f1503a85671ee8
mikolaj <mikolaj.muller23@gmail.com>
2019-09-02 18:02:15 +0200

    test

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var app = express();
 
 app.use(express.static('dist'));
 
-var server = app.listen(8081, function () {
+var server = app.listen(3000, function () {
    var host = server.address().address
    var port = server.address().port
 
